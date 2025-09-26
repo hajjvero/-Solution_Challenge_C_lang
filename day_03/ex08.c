53b9a8ef94b58320120e875115dbb91c883eaaff
hh.hajjaji.hamza <hh.hajjaji.hamza@gmail.com>
2025-09-26 08:30:14 +0100

    add part one of ec 8

--- day_03/ex08.c
+++ day_03/ex08.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
+    char phrase[128], temp[128];
 
+    printf("Entrez une phrase : ");
+    scanf("%[^\n]", phrase);
+
+    int length = strlen(phrase);
+    // . ? ! , '
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
 }
